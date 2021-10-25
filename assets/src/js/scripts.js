590e5684e9acb1e4bb8b48270b48215fbac11f3e
ariesrei <arsrymeralles@gmail.com>
2021-10-25 08:23:19 +0800

    added GSAP on HTML

--- assets/src/js/scripts.js
+++ assets/src/js/scripts.js
@@ -25,3 +25,70 @@ mybutton.addEventListener("click", function() {
 });
 
 $(".current_page_item a").addClass("active");
+
+ 
+// GSAP
+// gsap.from('.img-container', {opacity: 0, duration: 1, x: -100});
+ 
+//gsap.from('.media', {opacity: 0, duration: 1,y: 100});
+ 
+
+
+// let tween = gsap.from(".media", {
+//   opacity: 0,
+//   y:100, 
+//   scrollTrigger: ".media"
+// });
+
+//console.log(ScrollTrigger.getById("example").animation); // tween
+
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromBottom")) {
+    x = 0;
+    y = 100;
+  } else if (elem.classList.contains("gs_reveal_fromTop")) {
+    console.log('top');
+    x = 0;
+    y = -100;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.25, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
